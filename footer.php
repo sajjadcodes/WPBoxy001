5c2c5d26025434f9b943e779c7dec43cf48f71de
sajadhussain <sakraan01@gmail.com>
2020-06-29 02:24:58 +0500

    gulp updated

--- footer.php
+++ footer.php
@@ -28,6 +28,7 @@ $container = get_theme_mod( 'wpboxy_container_type' );
 					<div class="site-info">
 
 						<?php wpboxy_site_info(); ?>
+						<?php _e('This is the testing','wpboxy'); ?>
 
 					</div><!-- .site-info -->
 
